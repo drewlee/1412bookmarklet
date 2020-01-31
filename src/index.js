b61a570a99f93a7833d4ec75c70cc24ac894c6b4
drewlee <starsthatfell@gmail.com>
2020-01-30 23:24:49 -0800

    Fixes Firefox bug

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@
   if (typeof w[name] !== 'undefined') {
     document.head.removeChild(w[name].instance);
     delete w[name];
-    return;
+  } else {
+    style = document.createElement('style'),
+    w[name] = {};
+    w[name].instance = style;
+    document.head.appendChild(style);
+    sheet = style.sheet;
+    sheet.insertRule('*{line-height:1.5!important;}');
+    sheet.insertRule('*{letter-spacing:0.12em!important;}');
+    sheet.insertRule('*{word-spacing:0.16em!important;}');
+    sheet.insertRule('p{margin-bottom:2em!important;}');
   }
-
-  style = document.createElement('style'),
-  w[name] = {};
-  w[name].instance = style;
-  document.head.appendChild(style);
-  sheet = style.sheet;
-  sheet.insertRule('*{line-height:1.5!important;}');
-  sheet.insertRule('*{letter-spacing:0.12em!important;}');
-  sheet.insertRule('*{word-spacing:0.16em!important;}');
-  sheet.insertRule('p{margin-bottom:2em!important;}');
 })(window);
